8b965a68581fdbf8a6c65b4031b04c7fd3b074dc
mrbarazi-97 <mrbarazi.97@gmail.com>
2023-02-08 17:09:22 +0330

    Day-1

---
+++ day-1/main.py
@@ -0,0 +1,15 @@
+#print("whats is "print() function"") wrong code
+print("whats is 'print() function'") # this is true
+print('whats is "print() function"') # this also is true
+
+
+print("Hi there\n My name is mohammad \nand I'm trying to learn code with python")
+
+print("Hello" + input("What is your name ?"))
+
+print(len(input("what is your name ?"))) # out is number of your character name
+
+name = input("what is your name ?")
+numOfName = len(name)
+#print(name + "have" + numOfName + "character name") #Wrong
+print( name +"have" + str(numOfName) + "character name") #True
